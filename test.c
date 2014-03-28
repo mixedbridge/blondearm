3d8f5630c23b6e16ad25b274334bc802461a3165
mixedbridge <nehemiah.moses+github@gmail.com>
2014-03-28 23:39:52 +0800

    Completed base64 to hex decoder in test.c

--- test.c
+++ test.c
@@ -1,29 +1,92 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <assert.h>
+#include <stdint.h>
 
-void b642int(char *input_buffer, char *output_buffer, size_t *output_size){
-	
+static char encoding_table[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
+                                'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+                                'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
+                                'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f',
+                                'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
+                                'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+                                'w', 'x', 'y', 'z', '0', '1', '2', '3',
+                                '4', '5', '6', '7', '8', '9', '+', '/'};
+static char *decoding_table = NULL;
+
+void build_decoding_table() {
+
+	decoding_table = malloc(256);
+
+	int i = 0;
+	for (i = 0; i < 64; i++)
+		decoding_table[(unsigned char) encoding_table[i]] = i;
+}
+
+void padb64(char *input_buffer){
+	/* Takes an input char buffer
+	 * and makes it hold a multiple of 4 characters
+	 * missing characters are padded with '='
+	 * can result in 3 '=' at the end
+	 * which can never be the case if 
+	 * input is actually delivered in base64
+	 */
+
 	size_t input_size = strlen(input_buffer);
-	size_t input_mod = input_size % 4;
-	printf("Old Input Size: %zu\n", input_size);
-
 	size_t pad_input_size = input_size;
-	if (input_mod > 0) pad_input_size = input_size - input_mod + 4;
+	size_t input_mod = input_size % 4;
 
-	printf("New Input Size: %zu\n", pad_input_size);
+	if (input_mod > 0) pad_input_size = input_size - input_mod + 4;
 
 	int i = 0;
 	if (input_size < pad_input_size){
 		input_buffer = realloc((void *)input_buffer, pad_input_size);
-		printf("Strlen: %zu\n", strlen(input_buffer));
 		for (i = 0; i < (4 - input_mod); i++){
 			input_buffer[input_size + i] = '=';
 		}
 	}
-	printf("Strlen: %zu\n", strlen(input_buffer));
-	*output_size = 3 * pad_input_size / 4;
-	output_buffer = malloc(*output_size);
+	input_buffer[pad_input_size] = '\0';
+
+	printf("%s\n", input_buffer);
+}
+
+void b642int(char *input_buffer, unsigned char **output_buffer, size_t *output_size){
+	
+	padb64(input_buffer);
+	size_t input_size = strlen(input_buffer);
+	assert((input_size % 4) == 0);
+	
+	*output_size = input_size / 4 * 3;
+	if (input_buffer[input_size - 1] == '=') (*output_size)--;
+	if (input_buffer[input_size - 2] == '=') (*output_size)--;
+
+	*output_buffer = malloc(*output_size);
+	build_decoding_table();
+
+	int i = 0, j = 0;
+	for (i = 0, j = 0; i < input_size;){
+		uint32_t sextet_a = input_buffer[i] == '=' ? 0 & i++ : 
+			decoding_table[input_buffer[i++]];
+		
+		uint32_t sextet_b = input_buffer[i] == '=' ? 0 & i++ : 
+			decoding_table[input_buffer[i++]];
+
+		uint32_t sextet_c = input_buffer[i] == '=' ? 0 & i++ : 
+			decoding_table[input_buffer[i++]];
+
+		uint32_t sextet_d = input_buffer[i] == '=' ? 0 & i++ : 
+			decoding_table[input_buffer[i++]];
+
+		uint32_t triple = (sextet_a << 3 * 6)
+			+ (sextet_b << 2 * 6)
+			+ (sextet_c << 1 * 6)
+			+ (sextet_d << 0 * 6);
+
+		if (j < *output_size) (*output_buffer)[j++] = (triple >> 2 * 8) & 0xFF;
+		if (j < *output_size) (*output_buffer)[j++] = (triple >> 1 * 8) & 0xFF;
+        if (j < *output_size) (*output_buffer)[j++] = (triple >> 0 * 8) & 0xFF;
+	}
+	(*output_buffer)[(*output_size)] = '\0';
 }
 
 int main(){
@@ -35,7 +98,7 @@ int main(){
 	char *b64_buffer = NULL;
 	size_t b64_length = 0;
 
-	char *int_buffer = NULL;
+	unsigned char *int_buffer = NULL;
 	size_t int_size = 0;
 
 	ssize_t chars_read = -1;
@@ -52,12 +115,19 @@ int main(){
 			 * that processes input buffer
 			 * and returns a buffer of ints
 			 */
-			b642int(b64_buffer, int_buffer, &int_size);
+			b642int(b64_buffer, &int_buffer, &int_size);
 			printf("Input Gets %zu Bytes\n", int_size);
+			
+			printf("Output: ");
+		
+			int i = 0;
+			for (i = 0; i < int_size; i++)
+				printf("%x", int_buffer[i]);
+			printf("\n");
 		}
-
 	}
 	
 	free(b64_buffer);
 	free(int_buffer);
+	free(decoding_table);
 }
